cbd22d9c86c2c93bc7e53ed847a9c9838d574a5b
Bernhard Fluer <bernhard.fluer@cancom.com>
2025-09-17 15:47:14 +0200

    updated api version

--- modules/roleassignment/main.tf
+++ modules/roleassignment/main.tf
@@ -2,7 +2,7 @@ data "azapi_resource_list" "role_definitions" {
   count = var.role_assignment_definition_lookup_enabled ? 1 : 0
 
   parent_id = var.role_assignment_scope
-  type      = "Microsoft.Authorization/roleDefinitions@2022-04-01"
+  type      = "Microsoft.Authorization/roleDefinitions@2025-04-01"
   response_export_values = {
     results = "value[].{id: id, role_name: properties.roleName}"
   }
