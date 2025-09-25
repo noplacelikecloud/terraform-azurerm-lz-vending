cdab953b2f5a682904e07b52e317804c4e7c3449
Bernhard Fluer <bernhard.fluer@cancom.com>
2025-09-25 14:19:56 +0200

    bugfix -- failure in calling

--- main.recoveryservicesvault.tf
+++ main.recoveryservicesvault.tf
@@ -3,13 +3,13 @@ module "rsv" {
 
   for_each = { for rsv_k, rsv_v in var.recovery_services_vaults : rsv_k => rsv_v if var.recovery_services_vault_enabled }
 
-  name     = rsv_v.name
-  location = coalesce(rsv_v.location, var.location)
+  name     = each.value.name
+  location = coalesce(each.value.location, var.location)
   resource_group_name = coalesce(
-    can(module.resourcegroup[rsv_v.resource_group_key].resource_group_name) ? module.resourcegroup[rsv_v.resource_group_key].resource_group_name : null,
-    rsv_v.resource_group_name_existing != null ? rsv_v.resource_group_name_existing : null
+    can(module.resourcegroup[each.value.resource_group_key].resource_group_name) ? module.resourcegroup[each.value.resource_group_key].resource_group_name : null,
+    each.value.resource_group_name_existing != null ? each.value.resource_group_name_existing : null
   )
-  sku                 = rsv_v.sku
-  tags                = rsv_v.tags
-  soft_delete_enabled = rsv_v.soft_delete_enabled
+  sku                 = each.value.sku
+  tags                = each.value.tags
+  soft_delete_enabled = each.value.soft_delete_enabled
 }
