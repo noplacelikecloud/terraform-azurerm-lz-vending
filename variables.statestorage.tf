fc095cdedf14f815317476449e2a03dcf740d85c
Bernhard Fluer <bernhard.fluer@cancom.com>
2025-11-04 14:35:26 +0100

    fix: add default value for lv4_statestorage_account_name variable

--- variables.statestorage.tf
+++ variables.statestorage.tf
@@ -1,6 +1,7 @@
 variable "lv4_statestorage_account_name" {
     description = "The name of the state storage account for level 4 deployments."
     type        = string
+    default     = ""
 }
 
 variable "deploy_lv4_statestorage_account" {
