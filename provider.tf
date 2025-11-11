57775f76bf8df5cd19d7e1844d64c876b7911286
Bernhard Fluer <bernhard.fluer@cancom.com>
2025-11-11 17:41:16 +0100

    feat: reintroduce azurerm provider configuration in provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    features {}
+
+    subscription_id = "38aabd5e-ae4d-4d73-adc3-0311f6a59a6e" #dummy subscription
+    resource_provider_registrations = "none"
+  
+}
--- providers.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-    features {}
-    
-    # Ensure the provider uses the subscription ID from the local variable
-    subscription_id = local.subscription_id
-    resource_provider_registrations = "none"   
-}
